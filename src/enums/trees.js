e79454945503f4fe02b228bed810d87f2278541f
gabriel <gabriel.chimifosk@gmail.com>
2025-12-07 18:02:31 -0300

    change colors

--- src/enums/trees.js
+++ src/enums/trees.js
@@ -1,13 +1,12 @@
 export const TREES = [
-    { bg: "bg-red-600", border: "border-yellow-400" },
-    { bg: "bg-purple-600", border: "border-neutral-900" },
-    { bg: "bg-blue-600", border: "border-neutral-900" },
-    { bg: "bg-green-600", border: "border-lime-400" },
-    { bg: "bg-indigo-700", border: "border-purple-300" },
-    { bg: "bg-teal-700", border: "border-cyan-300" },
-    { bg: "bg-amber-700", border: "border-yellow-200" },
-    { bg: "bg-violet-800", border: "border-fuchsia-300" },
-    { bg: "bg-orange-600", border: "border-yellow-300" },
-    { bg: "bg-rose-700", border: "border-pink-300" },
-  ];
-  
+  { bg: "bg-red-700", border: "border-neutral-900" },
+  { bg: "bg-purple-800", border: "border-neutral-900" },
+  { bg: "bg-sky-700", border: "border-neutral-900" },
+  { bg: "bg-pink-800", border: "border-neutral-900" },
+  { bg: "bg-indigo-700", border: "border-neutral-900" },
+  { bg: "bg-teal-700", border: "border-neutral-900" },
+  { bg: "bg-amber-700", border: "border-neutral-900" },
+  { bg: "bg-violet-800", border: "border-neutral-900" },
+  { bg: "bg-orange-600", border: "border-neutral-900" },
+  { bg: "bg-green-600", border: "border-neutral-900" },
+];
